9df0d2072a0154d7b9da8d2917d51a65d248843b
M3ngX <2558253003@qq.com>
2023-10-06 07:39:41 +0800

    添加了用户登录界面

--- routes.go
+++ routes.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go+vue/controller"
+)
+
+func CollectRoute(r *gin.Engine) *gin.Engine {
+	r.POST("/api/auth/register", controller.Reginster)
+	r.POST("/api/auth/login", controller.Login)
+	return r
+}
